190a5a25460f73789a0b2dc22443b7f41e710b52
David Harris <davlsu14@gmail.com>
2021-04-02 11:59:35 -0500

    Finsished exercise

---
+++ Operatorsexercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Operatorsexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            
+           
+
+            Console.WriteLine("What is the radius of your circle?");
+            var pi = Math.PI;
+            var r = double.Parse(Console.ReadLine());
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($" The area of a circle with radius of {r} is {areaOfCircle}");
+        }
+    }
+}
